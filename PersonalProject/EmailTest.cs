112556a31c06680c55fed41304c325ff0458d204
birdLove <hbradt@uci.edu>
2021-12-11 15:25:29 -0800

    Programming file

--- PersonalProject/EmailTest.cs
+++ PersonalProject/EmailTest.cs
@@ -15,13 +15,17 @@ namespace PersonalProject
         {
             String emailListFile = Credentials.DocumentsFolder+@"HalBookApp\EmailList.txt";
             DirectoryInfo dir = new DirectoryInfo(Credentials.DocumentsFolder + @"HalBookApp");
-
+            String body = "To whom it may concern\n Please check the attachment below!\nThanks, Haley B";
             FileInfo file = null;
             foreach (var f in dir.GetFiles())
             {
-                if(f.FullName.Contains(DateTime.Now.ToString("MMddYYYY")) || f.FullName.Contains(DateTime.Now.ToString("MMddyy")))
+                if ((f.FullName.Contains(DateTime.Now.ToString("MMddYYYY")) || f.FullName.Contains(DateTime.Now.ToString("MMddyy"))) && !f.FullName.ToLower().Contains("body"))
                 {
                     file = new FileInfo(f.FullName);
+                }
+                if ((f.FullName.Contains(DateTime.Now.ToString("MMddYYYY")) || f.FullName.Contains(DateTime.Now.ToString("MMddyy"))) && f.FullName.ToLower().Contains("body"))
+                {
+                    body = File.ReadAllText(f.FullName);
                 }
             }
 
@@ -31,7 +35,7 @@ namespace PersonalProject
                 if (file!=null && file.Exists)
                 {
                     if (EmailFileRead.ValidateEmail(e))
-                        EmailFileRead.EmailTestResultsEmail(e,DateTime.Now.ToString("MMdd")+" Important","To whom it may concern\n Please check the attachment below!\nThanks, Haley B",file.FullName);
+                        EmailFileRead.EmailTestResultsEmail(e,DateTime.Now.ToString("MMdd")+" Important",body,file.FullName);
                 }
                 else
                 {
