73873cc6e284a87181c43474a1c887d044408b56
birdLove <hbradt@uci.edu>
2021-12-12 10:21:34 -0800

    commit 12/12

--- PersonalProject/EmailTest.cs
+++ PersonalProject/EmailTest.cs
@@ -45,6 +45,49 @@ namespace PersonalProject
             }
         }
 
+        [Test, Category("Sample Send Message"), Category("Sanity")] 
+        public void Text()
+        {
+            String textListFile = Credentials.DocumentsFolder + @"HalBookApp\TextList.txt";
+            DirectoryInfo dir = new DirectoryInfo(Credentials.DocumentsFolder + @"HalBookApp");
+            String body = "To whom it may concern\n Please check the attachment below!\nThanks, Haley B";
+            FileInfo file = null;
+            bool bodyExists = false;
+
+            foreach (var f in dir.GetFiles())
+            {
+                if ((f.FullName.Contains(DateTime.Now.ToString("MMddYYYY")) || f.FullName.Contains(DateTime.Now.ToString("MMddyy"))) && !f.FullName.ToLower().Contains("body"))
+                {
+                    file = new FileInfo(f.FullName);
+                }
+                if ((f.FullName.Contains(DateTime.Now.ToString("MMddYYYY")) || f.FullName.Contains(DateTime.Now.ToString("MMddyy"))) && f.FullName.ToLower().Contains("body"))
+                {
+                    body = File.ReadAllText(f.FullName);
+                    bodyExists = true;
+                }
+            }
+
+            var text = EmailFileRead.GetTextsFromFile(textListFile);
+            foreach (var e in text)
+            {
+                if (file != null && file.Exists)
+                {
+                    if (EmailFileRead.ValidateText(e))
+                        EmailFileRead.SendText(e, DateTime.Now.ToString("MMdd") + " Important", body, file.FullName);
+                }
+                else if(file == null && bodyExists)
+                {
+                    if (EmailFileRead.ValidateText(e))
+                        EmailFileRead.SendText(e, DateTime.Now.ToString("MMdd") + " Important", body, "");
+                }
+                else
+                {
+                    Console.WriteLine("Didn't send anything");
+                    File.WriteAllText(file.FullName, "You didn't send anything today!");
+                }
+            }
+        }
+
 
     }
 }
--- PersonalProject/Framework/EmailFileRead.cs
+++ PersonalProject/Framework/EmailFileRead.cs
@@ -17,6 +17,24 @@ namespace EmailReader //rename
 
         }
 
+        public static List<String> Carriers
+        {
+            get
+            {
+                List<String> carrier = new List<String>();
+                carrier.Add("@txt.att.net");
+                carrier.Add("@vtext.com");
+                carrier.Add("@messaging.sprintpcs.com");
+                carrier.Add("@tmomail.net");
+                carrier.Add("@vmobl.com");
+                carrier.Add("@messaging.nextel.com");
+                carrier.Add("@myboostmobile.com");
+                carrier.Add("@message.alltel.com");
+                carrier.Add("@mms.ee.co.uk");
+                return carrier;
+            }
+        }           
+
         public static String ReadText(String fileName = "")
         {
             return File.ReadAllText(fileName);
@@ -33,6 +51,21 @@ namespace EmailReader //rename
         public static bool ValidateEmail(String email = "")
         {
             return email.Contains("@") && email.Contains(".") && email!="";
+        }
+
+        public static String[] GetTextsFromFile(String fileName = "")
+        {
+            var textList = File.ReadAllText(fileName);
+            textList = textList.Replace("\n", "");
+            textList = textList.Replace(" ", "");
+            return textList.Split(',');
+        }
+
+        public static bool ValidateText(String text = "")
+        {
+            long i;
+            bool result = long.TryParse(text, out i);
+            return result && text != "";
         }
 
         public static void EmailTestResultsEmail(String email, String file = "")
@@ -62,6 +95,39 @@ namespace EmailReader //rename
             {
                 Console.WriteLine(ex.ToString());
             }
+        }
+
+        public static bool SendText(String text, String subject, String body, String file = "")
+        {
+            foreach (var v in Carriers)
+            {
+                try
+                {
+                    var message = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                    message.From = new MailAddress(Credentials.SMTPEmail);
+                    var textAddress = text + v;
+
+                    message.To.Add(new MailAddress(text + v));
+                    message.Subject = subject;
+                    message.Body = body;
+                    if (file != "")
+                    {
+                        Attachment a = new Attachment(file);
+                        message.Attachments.Add(a);
+                    }
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(Credentials.SMTPEmail, Credentials.SMTPPassword);
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(message);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return true;
         }
 
         public static void EmailTestResultsEmail(String email, String subject, String body, String file = "")
